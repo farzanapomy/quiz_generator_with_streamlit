877f0ea8d81c3e54cdf8bfed4ac2783288e7b933
Farzana Pomy <86626518+farzanapomy@users.noreply.github.com>
2026-04-19 23:33:29 +0600

    Add files via upload

--- api_calling.py
+++ api_calling.py
@@ -1,4 +1,4 @@
-from google import genai
+import google.genai as genai
 from dotenv import load_dotenv
 from gtts import gTTS
 import os
@@ -10,32 +10,42 @@ api_key = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key = api_key)
 
-def generate_note( images):
+def generate_note(images):
     prompt = """Summarize the picture in note formate at max 100 words make sure to add necessary markdown to differentiate different section in Bangla"""
+    
+    try:
+        res = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents=[images, prompt]
+        )
+        return res.text
 
-    res = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents=[images, prompt]   
-    )
-    return res.text
+    except Exception as e:
+        return f"Error generating note: {str(e)}"
 
 
 
 # audio transcription
 def generate_audio( text ):
-    speech = gTTS(text =text , lang='en', slow=False)
-    stream_buffer = io.BytesIO()
-    speech.write_to_fp(stream_buffer)
-    return stream_buffer
-
+    try:
+        speech = gTTS(text =text , lang='en', slow=False)
+        stream_buffer = io.BytesIO()
+        speech.write_to_fp(stream_buffer)
+        return stream_buffer
+    except Exception as e:
+        return f"Error generating audio: {str(e)}"
 
 # quiz generation
 
 def generate_quiz(images,  difficulty):
     prompt = f"Generate a {difficulty} quiz based on the content of the following images: {images} also add the markdown to differentiate the question and answer section"
 
-    res = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents=[images, prompt]   
-    )
+    try:
+        res = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents=[images, prompt]
+        )
+        return res.text
+    except Exception as e:
+        return f"Error generating quiz: {str(e)}"
     return res.text
